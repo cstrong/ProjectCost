c7834a909a586c4f4289eeb9fbe92282bcfaa8e0
Craeg Strong <cstrong@arielpartners.com>
2015-03-28 13:52:21 -0400

    started python flask implementation

--- ProjectCost.py
+++ ProjectCost.py
@@ -1,12 +1,35 @@
-from flask import Flask
+############################################
+#
+# Main ProjectCost application file
+#
+############################################
+
+from flask import Flask, current_app, render_template
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name, appname=current_app.name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
