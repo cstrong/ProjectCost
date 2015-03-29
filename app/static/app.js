db81ca1ce6105cb8c638234bc4a39f985a51a986
Craeg Strong <cstrong@arielpartners.com>
2015-03-29 15:08:15 -0400

    completed integration python + angular

--- app/static/app.js
+++ app/static/app.js
@@ -12,8 +12,8 @@ angular.module('app', [
   'defineProjectControllers'
 ]).constant('dataProvider', {url: 'http://'}).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/LandingPage', {templateUrl: 'landingPage/partials/landingPage.html', controller: 'LandingPageCtrl'});
-  $routeProvider.when('/Projects', {templateUrl: 'projects/partials/projects.html', controller: 'ProjectsCtrl'});
-  $routeProvider.when('/DefineProject', {templateUrl: 'defineProject/partials/defineProject.html', controller: 'DefineProjectCtrl'});
+  $routeProvider.when('/LandingPage', {templateUrl: 'static/landingPage/partials/landingPage.html', controller: 'LandingPageCtrl'});
+  $routeProvider.when('/Projects', {templateUrl: 'static/projects/partials/projects.html', controller: 'ProjectsCtrl'});
+  $routeProvider.when('/DefineProject', {templateUrl: 'static/defineProject/partials/defineProject.html', controller: 'DefineProjectCtrl'});
   $routeProvider.otherwise({redirectTo: '/LandingPage'});
 }]).value('activeMenu', {href: "#/LandingPage"});
